3765ca5e092f55eb51d089767beacd5f4aa4098b
WARIS MAQBOOL KHAN <119492366+WKMEDIAWORKS@users.noreply.github.com>
2025-10-03 12:36:29 +0530

    Initial commit: Add Singly linked list code

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(4);
+        list.insertFirst(18);
+        list.delete(-1);
+        list.display();
+    }
+}
